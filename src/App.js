c99878a809064429bbf3d277aaaf3cf35229e5e3
Cassio Pereira <cassioribeirop@gmail.com>
2022-09-12 11:45:54 -0300

    tela básica com react.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+    <h1>Hello world</h1>
+    <p>Com React Js</p>
+    <p className="autor"> Cassio Pereira</p>
+   </div>
   );
-}
-
-export default App;
+ }
+ 
+ export default App;
